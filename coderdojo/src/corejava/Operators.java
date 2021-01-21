effe1877177c7db5d524876123ce9a988aefaa90
sai <harikapulluru@gmail.com>
2021-01-20 20:34:52 -0800

    This is my first commit in java!

---
+++ coderdojo/src/corejava/Operators.java
@@ -0,0 +1,10 @@
+package corejava;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I have finally finished the GITHUB turtorial...or I think so.");
+	}
+
+}
