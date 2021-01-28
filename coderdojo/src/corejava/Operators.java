69257d9a074371f7e136bec2cc98f57b8ba45f1e
sai <harikapulluru@gmail.com>
2021-01-27 20:37:48 -0800

    first commit yay

--- coderdojo/src/corejava/Operators.java
+++ coderdojo/src/corejava/Operators.java
@@ -4,7 +4,22 @@ public class Operators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("I have finally finished the GITHUB turtorial...or I think so.");
+				int x = 65;
+				int y = 12;
+				int w = x+y;
+				int k = x-y;
+				int q = x*y;
+				int r = x/y;
+				int h = x%y;
+				System.out.println(w);
+				System.out.println(k);
+				System.out.println(q);	
+				System.out.println(r);	
+				System.out.println(h);
+	
+	
+	
+	
 	}
 
 }
