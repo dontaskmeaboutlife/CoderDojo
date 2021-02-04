c0f9e517eb9469fe70a466b9cf04d02d1b597553
sai <harikapulluru@gmail.com>
2021-02-03 20:06:31 -0800

    another commit

--- coderdojo/src/corejava/Main.java
+++ coderdojo/src/corejava/Main.java
@@ -1,6 +1,6 @@
 package corejava;
 
-public class Operators {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ coderdojo/src/corejava/Operatorsindepthforclasstopush.java
@@ -0,0 +1,39 @@
+package corejava;
+
+public class Operatorsindepthforclasstopush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//a few numbers
+		int x = 400;
+		int y = 504;
+		double a = 43.1932;
+		double z = 23.0934;
+		System.out.println("Variable Values....");
+		System.out.println("	x ="+ x);
+		System.out.println("	y ="+ y);
+		System.out.println("	a ="+ a);
+		System.out.println("	z ="+ z);
+		//adding numbers
+		System.out.println("Adding...");
+		System.out.println("	x + y = " + (x + y));
+		System.out.println("	a + z = "+ (a + z));
+		//subtracting numbers
+		System.out.println("Subtracting...");
+		System.out.println("	x - y =" + (x - y));
+		System.out.println("	a - z =" + (a - z));
+		//multiplying numbers
+		System.out.println("Multipying...");
+		System.out.println("	x * y =" + (x * y));
+		System.out.println("	a * z" + (a * z));
+		//dividing numbers
+		System.out.println("Dividing...");
+		System.out.println("	x / y =" + (x / y));
+		System.out.println("	a / z =" + ( a / z));
+		//computing the remainder resulting from dividing numbers
+		System.out.println("Computing the remainder...");
+		System.out.println("	x % y =" + (x % y));
+		System.out.println("	a % z = " + (a % z));
+	}
+
+}
