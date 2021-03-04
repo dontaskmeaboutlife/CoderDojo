78605a23b96795ae6fca197d60ef84d4e205c636
sai <harikapulluru@gmail.com>
2021-03-03 19:25:24 -0800

    pets commit

---
+++ pets/src/database/PetInfo.java
@@ -0,0 +1,25 @@
+package database;
+
+
+import model.Pet;
+
+
+public class PetInfo {
+	
+	 public Pet getPet(String breed) {
+		 
+		 if(breed == "Labrador Retriever");
+		
+		 	Pet Labrador = new Pet();
+		 	Labrador.breed = "Labrador Retriever";
+		 	Labrador.description = "The Lab makes his 25th straight appearance as the number one breed. And why not? This family-friendly, smart, and fun-loving breed is a favorite in households across America. The to please and gentle disposition also help them excel as guide dogs and search-and-rescue dogs.";
+		 	return Labrador;
+		 {
+	 
+	 	return null;
+}
+
+
+	 }
+	 
+}
