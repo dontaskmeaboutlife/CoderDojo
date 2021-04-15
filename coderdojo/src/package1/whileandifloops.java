c945f90d1a85e98870c0c7a57df74a0728088bee
sai <harikapulluru@gmail.com>
2021-04-14 20:33:27 -0700

    the homework commit lol

---
+++ coderdojo/src/package1/whileandifloops.java
@@ -0,0 +1,65 @@
+package package1;
+
+public class whileandifloops {
+
+	public static void main(String[] args) {
+		int x = 1;
+		while (x < 2)
+			System.out.println(x);
+		x++;
+		int y = 2;
+		while (y < 3)
+			System.out.println(y);
+		y++;
+		int z = 3;
+		while (z < 4)
+			System.out.println(z);
+		z++;
+		int a = 4;
+		while (a < 5)
+			System.out.println(a);
+		a++;
+		int f = 5;
+		while (f < 6)
+			System.out.println(f);
+		f++;
+		int b = 6;
+		while (b < 7) {
+			System.out.println(b);
+			b++;
+		}
+		int g = 7;
+		while (g < 8)
+			System.out.println(g);
+		g++;
+		int d = 8;
+		while (d < 9)
+			System.out.println(d);
+		d++;
+		int j = 9;
+		while (j < 10)
+			System.out.println(j);
+		j++;
+		int m = 10;
+		while (m < 11)
+			System.out.println(m);
+		m++;
+
+		int day = 24;
+		if (day > 24) {
+			System.out.println("The day is not 24th, 24th has passed.");
+		} else if (day < 24) {
+			System.out.println("The day is not 24th, 24th is yet to come.");
+		} else {
+			System.out.println("The day is the 24th.");
+		}
+	for(int i = 1; i < 50; i = i + 2)
+		System.out.println(i);
+	}
+}
+	
+	
+
+	
+
+
